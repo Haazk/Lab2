98615c8a0988bb7c5c8e7ff006c9948dcede9dec
Haaziq <hrba21.23@ichat.sp.edu.sg>
2024-04-29 15:58:44 +0800

    new bmi

--- bmi.py
+++ bmi.py
@@ -2,7 +2,7 @@ def calculate_bmi (height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
     bmi=weight/(height*height)
-    print("bmi="+str(bmi))
+    print("bmi is"+str(bmi))
     if bmi<18.5:
         return -1
     elif bmi>=18.5 and bmi<=25.0:
